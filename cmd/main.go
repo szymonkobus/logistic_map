1b186c61253f2280ab57d1299878645f9155cd7a
szymonkobus <36537361+szymonkobus@users.noreply.github.com>
2019-12-19 00:46:53 +0100

    add: multiple convergance points

--- cmd/main.go
+++ cmd/main.go
@@ -13,51 +13,51 @@ import(
 const limit int = 5000
 type point struct {x float64; y float64}
 
-func compare_end(l [limit]float64, i int, rng int) bool {
+func findCycle(l [limit]float64, i int) (bool, int) {
     val := l[i]
-    for j:=i-1; j>=0 && j>=i-rng; j-- {
+    for j:=i-1; j>=0; j-- {
         if l[j] == val{
-            return true
+            return true, j
         }
     }
-    return false
+    return false, -1
 }
 
-func convergence_points(k float64,
+func convergencePoints(k float64,
                         state float64,
                         p []point,
                         verbose bool) []point {
 
     trace := [limit]float64{}
     trace[0] = state
-    var last int
     if(verbose){ fmt.Println("k: ",k,"\ts: ",state,"\tlimit: ",limit) }
 
     for i := 1; i<limit; i++{
         state = k * state * (1 - state)
         trace[i] = state
         if(verbose){ fmt.Println("s: ", state) }
-        if( compare_end(trace, i, 10) ){
+        if found, idx := findCycle(trace, i); found {
             if(verbose){ fmt.Println("convergance", state, "\tsteps: ",i) }
-            last = i
+            for j := idx; j < i; j++ {
+                p = append(p, point{k, trace[j]})
+            }
             break
         }
     }
 
-    p = append(p, point{k, trace[last]})
     return p
 }
 
-func run_simulations() []point {
+func RunSimulation() []point {
     var p []point
 
     state := 0.3
-    mink := 0.1
-    maxk := 3.9
-    step := 0.01
+    mink := 3.00
+    maxk := 3.99
+    step := 0.001
 
     for k := mink; k < maxk; k+=step {
-        p = convergence_points(k,state,p,false)
+        p = convergencePoints(k,state,p,false)
     }
 
     return p
@@ -72,7 +72,7 @@ func points2plotter(p []point) plotter.XYs {
     return r
 }
 
-func plot_points(p []point) {
+func PlotPoints(p []point) {
     data := points2plotter(p)
     diagram, err := plot.New()
 	if err != nil {
@@ -90,7 +90,6 @@ func plot_points(p []point) {
     scatter.GlyphStyle.Color = color.RGBA{R: 255, B: 128, A: 255}
     scatter.GlyphStyle.Shape = draw.CircleGlyph{}
     scatter.GlyphStyle.Radius = vg.Points(1)
-    fmt.Println(scatter)
 
 
     diagram.Add(scatter)
@@ -100,10 +99,10 @@ func plot_points(p []point) {
 }
 
 func main() {
-    //var p = []point {{0,0}, {1,1}, {0.5,0.75},} //sample points
-    p := run_simulations()
-    //points := convergence_points()
-    plot_points(p)
+    fmt.Println("Running simulation.")
+    p := RunSimulation()
+    fmt.Println("Plotting points.")
+    PlotPoints(p)
 }
 
 /*
